79120e7ae86b49798c4563d49ea7781233988fbf
SYYnb666 <3638506532@qq.com>
2025-10-26 22:34:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 def isOdd(param):
     # 1. 先判断参数是否为整数类型；2. 再判断该整数是否为奇数
     return isinstance(param, int) and param % 2 != 0
+param = input("请输入一个参数：")
+if isOdd(param):
+    print("Truse")
+else:
+    print("False")
